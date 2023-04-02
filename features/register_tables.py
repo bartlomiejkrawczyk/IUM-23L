fb7724421721450b9ef662cb6121a44e93e8bc7a
Bartłomiej Krawczyk <01158771@pw.edu.pl>
2023-04-02 16:00:29 +0200

    Add more columns when converting tables

--- features/register_tables.py
+++ features/register_tables.py
@@ -3,7 +3,7 @@ from config import VIEWS
 
 from pyspark.sql.dataframe import DataFrame
 import pyspark.sql.functions as F
-# import pyspark.sql.types as T
+import pyspark.sql.types as T
 
 from typing import List
 
@@ -22,13 +22,22 @@ def register_tables() -> None:
     for view, file in VIEWS.items():
         spark.sql(f'DROP TABLE IF EXISTS {view}').show()
         df = spark.read.json(file)
-        # for column in get_columns_of_type(df, 'boolean'):
-        #     df = df.withColumn(column, F.col(column).cast(T.IntegerType()))
+        for column in get_columns_of_type(df, 'boolean'):
+            df = df.withColumn(
+                f"{column}_numerical",
+                F.col(column).cast(T.IntegerType())
+            )
 
         for column in df.columns:
             if column in ['timestamp', 'release_date']:
-                df = df.withColumn(f'{column}_s', F.unix_timestamp(
-                    column, "yyyy[-MM-dd['T'HH:mm:ss[.SSSSSS]]]"))
+                df = df.withColumn(
+                    f'{column}_s',
+                    F.unix_timestamp(
+                        column,
+                        "yyyy[-MM-dd['T'HH:mm:ss[.SSSSSS]]]"
+                    )
+                )
+                df = df.withColumn(column, F.col(column).cast('timestamp'))
         df.write.mode('overwrite').saveAsTable(view, mode='overwrite')
 
 
