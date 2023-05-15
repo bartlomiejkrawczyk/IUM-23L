3353a1100b40bdf1143c0321c6620c401f906162
Bartłomiej Krawczyk <01158771@pw.edu.pl>
2023-05-16 00:30:01 +0200

    Update microservice

--- microservice.py
+++ microservice.py
@@ -1,9 +1,10 @@
 from flask import Flask, request, jsonify
 from sklearn.pipeline import Pipeline
 from sklearn.base import ClassifierMixin
-from io import StringIO
+from pandas.io.json import json_normalize
 import pandas as pd
 import pickle
+from typing import Dict
 
 app = Flask(__name__)
 
@@ -12,33 +13,39 @@ TARGETS = [
     "will_buy_premium_next_month_numerical"
 ]
 
+
 class IUMModel:
     pipeline: Pipeline
-    model: ClassifierMixin
-    threshold: float
+    predictors: Dict[str, ClassifierMixin]
+    thresholds: Dict[str, float]
 
-    def __init__(self, pipeline, model, threshold):
+    def __init__(self, pipeline: Pipeline, predictors: Dict[str, ClassifierMixin], thresholds: Dict[str, float]):
         self.pipeline = pipeline
-        self.model = model
-        self.threshold = threshold
+        self.predictors = predictors
+        self.thresholds = thresholds
 
 
-@app.route("/predict", methods=['POST'])
-def main():
-    with open("models/xgbclassifier.pkl", "rb") as f:
+@app.route("/predict/", methods=['POST'], defaults={'predicting_model': 'xgbclassifier'})
+@app.route("/predict/<predicting_model>", methods=['POST'])
+def main(predicting_model: str):
+    with open(f"models/{predicting_model}.pkl", "rb") as f:
         model: IUMModel = pickle.load(f)
 
-        data = pd.io.json.json_normalize(request.json)
+        data = json_normalize(request.json)  # type: ignore
         normalized_data = model.pipeline.transform(data)
 
         data_frame = pd.DataFrame(normalized_data, columns=data.columns)
 
         prediction = {}
-        for i, target in enumerate(TARGETS):
-            prediction[target] = bool(model.model.predict_proba(data_frame)[:, i][0] > model.threshold)
+        for target in enumerate(TARGETS):
+            predictor = model.predictors[target]
+            probability: float = predictor.predict_proba(  # type: ignore
+                data_frame
+            )[0][1]
+            prediction[target] = bool(probability > model.thresholds[target])
 
         return jsonify(prediction)
 
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(debug=True)
