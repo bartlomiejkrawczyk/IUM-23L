c7ec55f30555c900daf293ed7af079dcd49704ea
Bartłomiej Krawczyk <01158771@pw.edu.pl>
2023-05-27 23:25:18 +0200

    Add random seed and move some of the code to the utility class

--- utility.py
+++ utility.py
@@ -1,6 +1,12 @@
+from typing import Any, Dict, Optional, Union
+
+import numpy as np
+import seaborn as sns
+from matplotlib import pyplot as plt
+from pandas import DataFrame
 from sklearn.dummy import DummyClassifier
 from sklearn.linear_model import LogisticRegression
-from typing import Any, Union
+from sklearn.metrics import confusion_matrix, roc_auc_score
 from xgboost import XGBClassifier
 
 FEATURES = [
@@ -36,6 +42,8 @@ TARGETS = [
     "will_buy_premium_next_month_numerical"
 ]
 
+TARGET_AND_FEATURES = TARGETS + FEATURES
+
 DUMMY = 'dummy'
 LOGISTIC_REG = 'logistic_regression'
 XGB = 'xgb_classifier'
@@ -50,3 +58,74 @@ Model = Union[
     LogisticRegression,
     XGBClassifier
 ]
+
+
+def plot_matrix(matrix: DataFrame) -> None:
+    plt.figure(figsize=(16, 16))
+
+    sns.heatmap(
+        matrix,
+        xticklabels=matrix.columns,  # type: ignore
+        yticklabels=matrix.columns,  # type: ignore
+        annot=True,
+        annot_kws={"fontsize": 7},
+        fmt=".0%",
+        vmin=-1,
+        vmax=1,
+    )
+
+    plt.show()
+
+
+def construct_dummy(X_train: DataFrame,
+                    y_train: DataFrame,
+                    params: Optional[Dict[str, Any]] = None) -> DummyClassifier:
+    return DummyClassifier().fit(X_train, y_train)
+
+
+def construct_logistic_reggression(X_train: DataFrame,
+                                   y_train: DataFrame,
+                                   params: Optional[Dict[str, Any]] = None) -> LogisticRegression:
+    return LogisticRegression().fit(X_train, y_train)
+
+
+def plot_confusion_matrix(models: Dict[str, Model], X_test: DataFrame, Y_test: DataFrame) -> None:
+    _, axs = plt.subplots(1, 2, figsize=(24, 10))  # type: ignore
+    for i, target in enumerate(TARGETS):
+        model = models[target]
+        y_predicted = model.predict(X_test)
+        y_true = Y_test[target]
+        roc_auc_score_value = roc_auc_score(y_true, y_predicted)
+        print(f"ROC AUC score for {target}: {roc_auc_score_value}")
+        matrix = confusion_matrix(y_true, y_predicted)
+        sns.heatmap(
+            matrix,
+            annot=True,
+            annot_kws={"fontsize": 30},
+            fmt='g',
+            xticklabels=["0", "1"],  # type: ignore
+            yticklabels=["0", "1"],  # type: ignore
+            ax=axs[i]  # type: ignore
+        )
+    plt.show()
+
+
+def retrieve_weights(model: Model) -> np.ndarray[np.float64]:
+    if isinstance(model, LogisticRegression):
+        return model.coef_[0]
+    if isinstance(model, XGBClassifier):
+        return model.feature_importances_
+    return np.zeros(len(FEATURES))
+
+
+def plot_feature_importances(models: Dict[str, Model]) -> None:
+    _, axs = plt.subplots(1, len(TARGETS), figsize=(
+        30, 10), constrained_layout=True)
+    for i, target in enumerate(TARGETS):
+        model = models[target]
+        columns = FEATURES
+        weights = retrieve_weights(model)
+        axs[i].barh(y=columns, width=weights, edgecolor="black")
+        axs[i].set_title(
+            f"Model: {type} - feature importances for target {target}")
+    plt.show()
